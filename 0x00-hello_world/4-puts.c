10c4bfd8bd012641dd9b4b8d05ff619e22706bd7
Elampy <ampaaemmanuella16@gmail.com>
2023-10-09 02:23:18 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+# ! / bin / bash
 #include <stdio.h>
 /**
  *main - Entry point
@@ -6,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	Return (0);
+	Return(0);
 }
